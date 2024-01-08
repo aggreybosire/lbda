65b2eda509e18b75004d37d0c44153e078c87be7
Aggrey Bosire <abosire86@gmail.com>
2024-01-09 02:57:44 +0300

    Update chat-section.tsx

--- my-app/frontend/app/components/chat-section.tsx
+++ my-app/frontend/app/components/chat-section.tsx
@@ -20,20 +20,22 @@ export default function ChatSection() {
   });
 
   return (
-    <div className="space-y-4 max-w-5xl w-full">
-      <ChatMessages
-        messages={messages}
-        isLoading={isLoading}
-        reload={reload}
-        stop={stop}
-      />
-      <ChatInput
-        input={input}
-        handleSubmit={handleSubmit}
-        handleInputChange={handleInputChange}
-        isLoading={isLoading}
-        multiModal={process.env.NEXT_PUBLIC_MODEL === "gpt-4-vision-preview"}
-      />
+    <div className="p-4 md:p-8 lg:p-12 xl:p-16 bg-gradient-to-b from-blue-200 to-blue-400">
+      <div className="max-w-screen-md mx-auto">
+        <ChatMessages
+          messages={messages}
+          isLoading={isLoading}
+          reload={reload}
+          stop={stop}
+        />
+        <ChatInput
+          input={input}
+          handleSubmit={handleSubmit}
+          handleInputChange={handleInputChange}
+          isLoading={isLoading}
+          multiModal={process.env.NEXT_PUBLIC_MODEL === "gpt-4-vision-preview"}
+        />
+      </div>
     </div>
   );
 }
